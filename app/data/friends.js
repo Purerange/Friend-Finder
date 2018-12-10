f840747378d0117347eea7ba7f26516cf826afc7
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2018-12-10 15:35:50 -0800

    server config'd, html routes established

---
+++ app/data/friends.js
@@ -0,0 +1,116 @@
+// DATA
+
+var friendsArray = [
+    {
+        name: "Zac Conely",
+        photo: "http://images5.fanpop.com/image/photos/31000000/haters-gonna-hate-random-31076705-550-413.jpg",
+        scores: [
+            "4",
+            "2",
+            "4",
+            "3",
+            "2",
+            "5",
+            "5",
+            "2",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: "Kitt Boyd",
+        photo: "https://memestatic.fjcdn.com/pictures/Random+titles+2_39d894_6611920.jpg",
+        scores: [
+            "5",
+            "3",
+            "1",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2",
+            "4",
+            "3"
+        ]
+    },
+    {
+        name: "Flannery Dunn",
+        photo: "https://www.randomlists.com/img/animals/llama.jpg",
+        scores: [
+            "1",
+            "3",
+            "3",
+            "3",
+            "4",
+            "2",
+            "4",
+            "3",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Chris D'elia",
+        photo: "https://wallpaperbrowse.com/media/images/750814.jpg",
+        scores: [
+            "2",
+            "2",
+            "3",
+            "3",
+            "3",
+            "2",
+            "4",
+            "3",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Melissa Foster",
+        photo: "https://404store.com/2017/12/08/VH-Random-duckhourse.jpg",
+        scores: [
+            "3",
+            "4",
+            "5",
+            "2",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Michael McCoy",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWDAZStGvdxYmvOHsUkW0XadrubGAXJU8bhQJGWnFwIbhYsPHahQ",
+        scores: [
+            "5",
+            "5",
+            "3",
+            "5",
+            "3",
+            "3",
+            "4",
+            "3",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Ron Weasley",
+        photo: "http://www.vitamin-ha.com/wp-content/uploads/2013/09/Random-Funny-11.jpg",
+        scores: [
+            "4",
+            "4",
+            "1",
+            "3",
+            "4",
+            "2",
+            "3",
+            "5",
+            "4",
+            "3"
+        ]
+    }
+];
